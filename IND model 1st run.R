b9842c3c1bce69c09acb719449e42686fb6c475e
neekro <smira.martin@gmail.com>
2017-04-08 00:07:25 +0200

    minor changes

--- IND model 1st run.R
+++ IND model 1st run.R
@@ -1,10 +1,11 @@
 rm(list = ls())
-library(rstan) 
+library(rstan)
 library(dplyr)
 
 
 ## Read  data
-setwd("c:/Users/Hynek/Dropbox/MU/NFC/Analýzy/IOWA kognitivní modely")
+setwd("D:/Dropbox/Dokumenty/Projekty/IOWA GAMBLING TASK/NfCC-IGT")
+# setwd("c:/Users/Hynek/Dropbox/MU/NFC/Analýzy/IOWA kognitivní modely")
 
 iow5 <- read.csv2("data/IOWA2015trials.csv")
 iow6 <- read.csv2("data/IOWA2016trials.csv")
@@ -13,11 +14,11 @@ iow6 <- read.csv2("data/IOWA2016trials.csv")
 data <- bind_rows(iow5, iow6)
 data <- tbl_df(data)
 
-data <- data %>% 
-  select(ID, trial, deck, difference) %>% 
-  # filter(ID == 222) %>% 
+data <- data %>%
+  select(ID, trial, deck, difference) %>%
+  # filter(ID == 222) %>%
   mutate(deck = as.numeric(deck),
-         difference = difference / 1000) %>% 
+         difference = difference / 1000) %>%
   arrange(ID, trial)
 
 nSubjs <- length(unique(data$ID))
@@ -28,11 +29,11 @@ net <- matrix(data$difference, nSubjs, nTrials, byrow = TRUE)  # Net outcomes
 
 
 # Input data for the model
-mydata <- list(choice = choice, n_s = nSubjs, n_t = nTrials, net = net) 
+mydata <- list(choice = choice, n_s = nSubjs, n_t = nTrials, net = net)
 
 # Model set-up
 rstan_options(auto_write = TRUE)
-options(mc.cores = parallel::detectCores() - 2) ## Hynek: minus 2 instead minus 1 
+options(mc.cores = parallel::detectCores() - 2) ## Hynek: minus 2 instead minus 1
 
 # Parameters to be monitored
 mypars <- c("A_ind", "w_ind", "a_ind", "c_ind")
@@ -44,13 +45,12 @@ stan_mod <- stan_model('models/pvl_d_IND_1g.stan')
 
 start = Sys.time()
 # Run iterations; output data is saved in object "samples"
-samples <- sampling(stan_mod, data = mydata, init = "random", pars = mypars, 
+samples <- sampling(stan_mod, data = mydata, init = "random", pars = mypars,
                 warmup = 2000, iter = 12000, thin = 2, chains = 10)
 
-end = Sys.time()                  
+end = Sys.time()
 end - start   # run time
 
-print(samples, digits=3)      
+print(samples, digits=3)
 # windows(20,1); traceplot(samples, ask=TRUE)
 save(samples, file="samples/IND_1st_run.Rdata")
-
